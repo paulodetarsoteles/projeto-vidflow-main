f62e415b2b024864efb425fc34b9288205f46e32
Paulo de Tarso <potassio@hotmail.com>
2024-03-02 22:59:07 -0300

    Identação

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ async function buscarEMostrarVideos(){
 
             containerVideos.innerHTML += `
             <li class="videos__item">
-            <iframe src="${element.url}" title="${element.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descricao-video">
-            <img class="img-canal" src="${element.imagem}" alt="Logo do Canal">
-            <h3 class="titulo-video">${element.titulo}</h3>
-            <p class="titulo-canal">${element.descricao}</p>
-            </div>
+                <iframe src="${element.url}" title="${element.titulo}" frameborder="0" allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img class="img-canal" src="${element.imagem}" alt="Logo do Canal">
+                    <h3 class="titulo-video">${element.titulo}</h3>
+                    <p class="titulo-canal">${element.descricao}</p>
+                </div>
             </li>
             `;
         })
