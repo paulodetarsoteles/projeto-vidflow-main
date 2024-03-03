a1d17ebb574ce1b83cde13aa2af891736ded7784
Paulo de Tarso <potassio@hotmail.com>
2024-03-02 22:58:14 -0300

    Async, awsait e blocos try/catch/finally

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-                .then(response => response.json())
-                .then((videos) => 
-                    videos.forEach((element) => {
-                        containerVideos.innerHTML += `
-                            <li class="videos__item">
-                                <iframe src="${element.url}" title="${element.titulo}" frameborder="0" allowfullscreen></iframe>
-                                <div class="descricao-video">
-                                    <img class="img-canal" src="${element.imagem}" alt="Logo do Canal">
-                                    <h3 class="titulo-video">${element.titulo}</h3>
-                                    <p class="titulo-canal">${element.descricao}</p>
-                                </div>
-                            </li>
-                        `;
-                    })
-                ).catch((erro) => {
-                    containerVideos.innerHTML = `
-                        <p>Ocorreu um erro: ${erro}</p>
-                    `;
-                });
+async function buscarEMostrarVideos(){
+
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+
+        videos.forEach((element) => {
+            if (videos.categoria == '')
+                throw new Error('Vídeo sem categoria');
+
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+            <iframe src="${element.url}" title="${element.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+            <img class="img-canal" src="${element.imagem}" alt="Logo do Canal">
+            <h3 class="titulo-video">${element.titulo}</h3>
+            <p class="titulo-canal">${element.descricao}</p>
+            </div>
+            </li>
+            `;
+        })
+    } catch (error){
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos:${error}</p>`
+    } finally {
+        // Apenas para fonte de pesquisa
+    }
+}
+
+buscarEMostrarVideos();
